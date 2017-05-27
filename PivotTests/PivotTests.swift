b097333609ed4949eb08dbce8219931ae25d5495
Devin Frenze <devinfrenze@devins-mbp.att.net>
2017-05-26 21:47:02 -0700

    Initial Commit

---
+++ PivotTests/PivotTests.swift
@@ -0,0 +1,36 @@
+//
+//  PivotTests.swift
+//  PivotTests
+//
+//  Created by Devin Frenze on 5/26/17.
+//  Copyright © 2017 Devin Frenze. All rights reserved.
+//
+
+import XCTest
+@testable import Pivot
+
+class PivotTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
