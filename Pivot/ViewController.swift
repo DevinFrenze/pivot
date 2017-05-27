c7d2b10f90569581d1867c4bd97dc7e9353601a9
Devin Frenze <devinfrenze@Devins-MBP.att.net>
2017-05-26 22:28:02 -0700

    refactored a little bit and realized there might be a typo when rotating to be parallel with gravity

--- Pivot/ViewController.swift
+++ Pivot/ViewController.swift
@@ -35,8 +35,7 @@ class ViewController: UIViewController {
         if let deviceMotion = motionManager.deviceMotion {
             // if there is a previous device motion, calculate the change since the last calculation
             if let previous = previousDeviceMotion {
-                let referenceVector = GLKVector3Make(0,1,0)
-                var deltaTheta = getGravityAxisRotationDelta(deviceMotion, previous, referenceVector)
+                var deltaTheta = getGravityAxisRotationDelta(deviceMotion, previous)
                 if deltaTheta.isNaN { deltaTheta = 0 }
                 total += deltaTheta
             }
@@ -47,35 +46,37 @@ class ViewController: UIViewController {
 
     //MARK: Private Methods
     
-    private func getGravityAxisRotationDelta(_ current: CMDeviceMotion,_ previous: CMDeviceMotion,_ referenceVector: GLKVector3) -> Float {
+    private func getGravityAxisRotationDelta(_ current: CMDeviceMotion,_ previous: CMDeviceMotion) -> Float {
+        let referenceVector = GLKVector3Make(0,1,0)
+    
         var currentRotationMatrix = castCMRotationMatrixToGLKMatrix3(current.attitude.rotationMatrix)
         var previousRotationMatrix = castCMRotationMatrixToGLKMatrix3(previous.attitude.rotationMatrix)
         
-        let angleBetweenGravityAndZ = angleDifference(castCMAccelerationToGLKVector3(current.gravity), GLKVector3Make(0, 0, 1))
-        let vertical = abs(angleBetweenGravityAndZ - Float(M_PI_2)) < Float(M_PI_4)
-        
         // if the vertical angle between the attitude of gravity and the z-axis is < 45 degrees
         // rotate the attitudes we're comparing 90 degrees along the x-axis
+        let angleBetweenGravityAndZ = angleDifference(castCMAccelerationToGLKVector3(current.gravity), GLKVector3Make(0, 0, 1))
+        let vertical = abs(angleBetweenGravityAndZ - Float(M_PI_2)) < Float(M_PI_4)
         if vertical {
             let rotation = GLKMatrix3MakeXRotation(Float(M_PI_2))
             currentRotationMatrix = GLKMatrix3Multiply(currentRotationMatrix, rotation)
             previousRotationMatrix = GLKMatrix3Multiply(previousRotationMatrix, rotation)
         }
         
+        // get the directional angle difference between the gravity vector of the current and previous frame
         let projection = projectAttitudeOntoGravityPlane( currentRotationMatrix, current.gravity, referenceVector)
         let previousProjection = projectAttitudeOntoGravityPlane( previousRotationMatrix, previous.gravity, referenceVector)
         return directionalAngleDifference(previousProjection, projection)
     }
     
     private func angleDifference(_ v1: GLKVector3,_ v2: GLKVector3) -> Float {
+        // the difference between two vectors is the inverse cosine of their dot product divided by the product of their lengths
         let dotProduct = GLKVector3DotProduct(v1, v2)
-        let jointMagnitudes = GLKVector3Length(v1) * GLKVector3Length(v2)
-        return acos(dotProduct / jointMagnitudes)
+        let lengthProduct = GLKVector3Length(v1) * GLKVector3Length(v2)
+        return acos(dotProduct / lengthProduct)
     }
     
     private func directionalAngleDifference(_ v1: GLKVector3,_ v2: GLKVector3) -> Float {
-        // compare both vectors to a vector -90 degrees around the z-axis from the first so we can tell
-        // what direction the angle of difference is
+        // the directional angle difference between two angles is the difference of their differences with a third angle
         let angleDirectionReferenceVector = extractVectorFromMatrix(
             GLKMatrix3Multiply(GLKMatrix3MakeZRotation(-Float(M_PI_2)), embedVectorInMatrix(v2))
         )
@@ -84,8 +85,14 @@ class ViewController: UIViewController {
     
     private func projectAttitudeOntoGravityPlane(_ rotationMatrix: GLKMatrix3,_ gravity: CMAcceleration,_ referenceVector: GLKVector3) -> GLKVector3 {
         // understand the current attitude of the phone in terms of how it projects onto the plan defined by gravity as the normal to the plane (the x-y plane)
-        var matrix = GLKMatrix3Multiply(rotationMatrix, getXRotation(gravity))
-        matrix = GLKMatrix3Multiply(matrix, getYRotation(gravity))
+        
+        // double check that gravityXRotation shouldn't use GLKMatrix3MakeXRotation
+        let gravityXRotation = GLKMatrix3MakeYRotation(Float(atan2(gravity.y, sqrt(pow(gravity.x,2) + pow(gravity.z,2)))))
+        let gravityYRotation = GLKMatrix3MakeYRotation(Float(atan2(gravity.x, gravity.z)))
+
+        // rotate the reference vector around the x and y axes to be parallel with gravity
+        var matrix = GLKMatrix3Multiply(rotationMatrix, gravityXRotation)
+        matrix = GLKMatrix3Multiply(matrix, gravityYRotation)
         matrix = GLKMatrix3Multiply(matrix, embedVectorInMatrix(referenceVector))
         
         // throw away the z-coordinate, since we only care about the angle in the x-y plane
@@ -107,16 +114,6 @@ class ViewController: UIViewController {
     
     private func castCMAccelerationToGLKVector3(_ acceleration: CMAcceleration) -> GLKVector3 {
         return GLKVector3Make(Float(acceleration.x), Float(acceleration.y), Float(acceleration.z))
-    }
-    
-    private func getYRotation(_ vector: CMAcceleration) -> GLKMatrix3 {
-        let yRotation = GLKMatrix3MakeYRotation(Float(atan2(vector.x, vector.z)))
-        return yRotation
-    }
-    
-    private func getXRotation(_ vector: CMAcceleration) -> GLKMatrix3 {
-        let xRotation = GLKMatrix3MakeYRotation(Float(atan2(vector.y, sqrt(pow(vector.x,2) + pow(vector.z,2)))))
-        return xRotation
     }
     
     // current UI is likely to change, don't worry about this method too much for now
